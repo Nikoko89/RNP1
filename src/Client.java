4fff5ce9f306cbe71d7d295afde72dea6fed407f
Niko Khaled <niko_khaled@icloud.com>
2016-10-24 22:18:31 +0200

    pre

---
+++ src/Client.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket clientSocket = new Socket("127.0.0.1", 6432);
+            clientSocket.setKeepAlive(true);
+            boolean living = true;
+            while (living) {
+                // Nachricht vom Benutzer an den Client
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+                // Nachricht vom Server an den Client
+                BufferedReader serverInput = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+                // Nachricht vom Client an den Server
+                DataOutputStream clientOutput = new DataOutputStream(clientSocket.getOutputStream());
+
+                System.out.println("Bitte Eingabe tätigen: ");
+                String eingabe = userInput.readLine()  + '\n';
+
+                clientOutput.writeUTF(eingabe);
+                //clientOutput.writeBytes(eingabe);
+                if (eingabe.equals("bye")) {
+
+                    living = false;
+                    serverInput.close();
+                    userInput.close();
+                    clientSocket.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
